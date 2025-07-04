0d48ce987cd73e4003b3c7013358827ff43d3e90
Andreu <andreumontagut@gmail.com>
2025-07-04 17:49:32 +0200

    Session 18

---
+++ kafka/kafka-producer/src/main/java/com/microservices/demo/kafka/producer/config/service/impl/TwitterKafkaProducer.java
@@ -0,0 +1,50 @@
+package com.microservices.demo.kafka.producer.config.service.impl;
+
+import com.microservices.demo.kafka.avro.model.TwitterAvroModel;
+import com.microservices.demo.kafka.producer.config.service.KafkaProducer;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import javax.annotation.PreDestroy;
+
+public class TwitterKafkaProducer implements KafkaProducer<Long, TwitterAvroModel> {
+
+    private static  final Logger LOGGER = LoggerFactory.getLogger(TwitterKafkaProducer.class);
+
+    private final KafkaTemplate<Long, TwitterAvroModel> kafkaProducerTemplate;
+
+    public TwitterKafkaProducer(KafkaTemplate<Long, TwitterAvroModel> kafkaProducerTemplate) {
+        this.kafkaProducerTemplate = kafkaProducerTemplate;
+    }
+    @PreDestroy
+    public void close(){
+        if(kafkaProducerTemplate != null){
+            LOGGER.info("Closing Kafka Producer");
+            kafkaProducerTemplate.destroy();
+        }
+    }
+
+    @Override
+    public void send(String topicName, Long key, TwitterAvroModel message) {
+        LOGGER.info("Sending message={} to topic={}", message, topicName);
+        ListenableFuture<SendResult<Long, TwitterAvroModel>> kafkaResultFuture = kafkaProducerTemplate.send(topicName, key, message);
+        kafkaResultFuture.addCallback(new  ListenableFutureCallback<SendResult<Long, TwitterAvroModel>>() {
+            @Override
+            public void onSuccess(SendResult<Long, TwitterAvroModel> result) {
+                RecordMetadata  metadata = result.getRecordMetadata();
+                LOGGER.debug("Received new metadata. Topic: {}; Partition {}; Offset {}; Timestamp {}, at time {}",
+                        metadata.topic(), metadata.partition(), metadata.offset(), metadata.timestamp(), System.nanoTime());
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+
+            }
+        });
+    }
+}
